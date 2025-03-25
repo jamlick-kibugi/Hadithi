4ec2955497437e6ff47983cfb40a5fe1baba7f0e
jamlick-kibugi <67996337+jamlick-kibugi@users.noreply.github.com>
2025-03-25 11:27:25 +0300

    Create CreateStory.js

---
+++ src/pages/CreatePage/CreateStory.js
@@ -0,0 +1,25 @@
+CreatePage/import React, { useState } from 'react'
+ 
+import { Box, Typography } from '@mui/material'
+ 
+import CreateOptions from '../../components/CreateOptions'
+import PersonalisedStoryForm from '../../components/PersonalisedStoryForm'
+import CreateStory from '../../components/CreateStory'
+import { useAppContext } from '../../context/appContext'
+import CollabStoryForm from '../../components/CollabStoryForm'
+ 
+const CreatePage = () => {
+  const {option} = useAppContext()
+ 
+  return (
+    <Box sx={{background:"white",padding:"20px",borderRadius:"20px", justifyContent:"center",display:"flex",background:"#f8fafc"}}>
+      {option==""?<CreateOptions  />:null}
+      {option=="Stories"?<PersonalisedStoryForm/>:null}
+      {option=="Collaborations"?<CollabStoryForm/>:null}
+      {/* <PersonalisedStoryForm/> */}
+      {/* <CreateStory/> */}
+    </Box>
+  )
+}
+
+export default CreatePage
